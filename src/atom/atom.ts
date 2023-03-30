50abefe80931595743c7caeecf92cae9a964e30d
GiRang Choi <choigirang@kakao.com>
2023-03-31 08:55:41 +0900

    feat: Recoil 전역 상태
    서버 or 로컬로 리팩토링 예정

--- src/atom/atom.ts
+++ src/atom/atom.ts
@@ -1,21 +1,42 @@
-import { atom, RecoilState, selector } from "recoil";
+import { atom } from "recoil";
 // Recoil 라이브러리의 타입 중 하나로 atom과 selector 모두를 포함하여
 // 사용되는 모든 상태 타입의 부모 타입이다.
 // RecoilState<boolean>은 boolean값을 저장하고 업데이트 한다.
 
-export const penAlter = atom({
-  key: "trueOrFalse",
+// Home 컴포넌트에서 사용 Btn
+export const penAlterState = atom({
+  key: "penAlterState",
   default: false,
 });
-
-export const rejectAlter = atom({
-  key: "rejectAlter",
-  default: false,
+export const stopAlterState = atom({
+  key: "stopAlterState",
+  default: true,
 });
 
-export const nameInput = atom({
-  key: "nameInput",
+// Main 컴포넌트에서 사용 Btn
+export const rejectAlterState = atom({
+  key: "rejectAlterState",
+  default: true,
+});
+
+// Main 이름 입력창
+export const nameInputState = atom({
+  key: "nameInputState",
   default: false,
+});
+export const tippingName = atom({
+  key: "tippingName",
+  default: "",
+});
+
+// Todo 추가 리스트
+export const itemState = atom({
+  key: "itemState",
+  default: [
+    { id: 1, room: "엄마방", tool: "청소기돌리기" },
+    { id: 2, room: "아빠방", tool: "청소기돌리기" },
+    { id: 3, room: "내방", tool: "청소기돌리기" },
+  ],
 });
 // selector - atom을 기반으로 파생된 값을 계산할 때 사용
 // const mySelector = selector({
