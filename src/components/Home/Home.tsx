e5670975ef601e44bcd3cbd14c9d75750f33a8c3
GiRang Choi <choigirang@kakao.com>
2023-03-31 08:58:59 +0900

    feat: Home 컴포넌트
    todoList 및 structure

---
+++ src/components/Home/Home.tsx
@@ -0,0 +1,42 @@
+import Structure from "./Structure";
+import TodoList from "./TodoList";
+import styled from "styled-components";
+import InsertBtn from "./InsertBtn";
+import { useRecoilState } from "recoil";
+import { penAlterState } from "../../atom/atom";
+
+export default function Home() {
+  const [penState, setPenState] = useRecoilState(penAlterState);
+
+  return (
+    <Container>
+      {!penState ? <></> : null}
+      <Template>
+        <TodoList />
+        <Structure />
+      </Template>
+      <InsertBtn />
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+`;
+
+const Template = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 50%;
+  min-width: 500px;
+  height: 65%;
+  background-color: #dddddd;
+  box-shadow: 0rem 0rem 2rem 0rem #4c4c4c;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1%;
+`;
