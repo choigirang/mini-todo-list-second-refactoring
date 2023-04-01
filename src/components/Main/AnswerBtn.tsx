36c14f625f8d5af4e81cb3f843bcf6135ec023dd
GiRang Choi <choigirang@kakao.com>
2023-04-01 23:48:54 +0900

    feat: 중간저장
    pen btn 완성
    stop btn 작성 중

---
+++ src/components/Main/AnswerBtn.tsx
@@ -0,0 +1,46 @@
+import styled from "styled-components";
+import { useRecoilState } from "recoil";
+import { rejectAlterState } from "../../atom/atom";
+import { Link } from "react-router-dom";
+
+export default function AnswerBtn() {
+  const [reject, setReject] = useRecoilState(rejectAlterState);
+  // Main 거절 btn 클릭 시, 나오는 알림창에서 알림창 닫기 btn
+
+  const alterChange = () => {
+    setReject(!reject);
+  };
+
+  return (
+    <Container>
+      <Link to="/Home">
+        <img src="./images/okBtn.png" alt="okBtn"></img>
+      </Link>
+      <img
+        src="./images/noBtn.png"
+        alt="noBtn"
+        onClick={() => alterChange()}
+      ></img>
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  display: flex;
+
+  > a > img {
+    width: 10rem;
+    cursor: pointer;
+    &:hover {
+      transform: rotate(10deg);
+    }
+  }
+
+  > img {
+    width: 10rem;
+    cursor: pointer;
+    &:hover {
+      transform: rotate(10deg);
+    }
+  }
+`;
