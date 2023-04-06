0504a913b89d177bf728d4c27e03ceb8e2831f93
GiRang Choi <choigirang@kakao.com>
2023-04-06 23:24:03 +0900

    feat: 하드코딩 줄이기 및 청소퍼센트 recoil => api
    각각의 room에 따라 className과 이미지 주소값 map,
    청소퍼센테이지 recoil => api 받아오기

--- src/components/Home/Structure.tsx
+++ src/components/Home/Structure.tsx
@@ -1,49 +1,71 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import { useRecoilValue } from "recoil";
 import { roomState } from "../../atom/atom";
+import axios from "axios";
+
+interface Room {
+  [key: string]: {
+    className: string;
+    imgName: string;
+  };
+}
+
+interface Clean {
+  room: string;
+  clean: number;
+}
 
 export default function Structure() {
-  const roomClean = useRecoilValue(roomState);
-  const keyName = ["엄마방", "아빠방", "내방", "누나방", "거실"];
+  // const roomClean = useRecoilValue(roomState);
+  // const keyName = ["엄마방", "아빠방", "내방", "누나방", "거실"];
+  // 객체로 관리하면 한 줄로 가능, 객체 key
+  const [cleanData, setCleanData] = useState<Clean[]>([]);
+
+  const roomInfo: Record<string, { className: string; imgName: string }> = {
+    엄마방: {
+      className: "momRoom",
+      imgName: "roomMom",
+    },
+    아빠방: {
+      className: "dadRoom",
+      imgName: "roomDad",
+    },
+    내방: {
+      className: "myRoom",
+      imgName: "roomMy",
+    },
+    누나방: {
+      className: "sisRoom",
+      imgName: "roomSis",
+    },
+    거실: {
+      className: "livRoom",
+      imgName: "",
+    },
+  };
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:4000/clean")
+      .then((res) => {
+        setCleanData(res.data);
+      })
+      .catch((err) => console.error(err));
+  }, [cleanData]);
 
   return (
     <Container>
-      {keyName.map((name) => (
-        <div
-          key={name}
-          className={
-            name === "엄마방"
-              ? "momRoom"
-              : name === "아빠방"
-              ? "dadRoom"
-              : name === "내방"
-              ? "myRoom"
-              : name === "누나방"
-              ? "sisRoom"
-              : name === "거실"
-              ? "livRoom"
-              : ""
-          }
-        >
-          {name}: {roomClean.find((room) => room[name])?.[name]}
-          {/* <img
-            src={`./images/${
-              name === "엄마방"
-                ? "roomMom"
-                : name === "아빠방"
-                ? "roomDad"
-                : name === "내방"
-                ? "roomMy"
-                : name === "누나방"
-                ? "roomSis"
-                : name === "거실"
-                ? ""
-                : ""
-            }.png`}
-          ></img> */}
-        </div>
-      ))}
+      {cleanData.map((data) => {
+        const { room, clean } = data;
+        const { className, imgName } = roomInfo[room];
+        return (
+          <div key={room} className={className}>
+            {room} : {clean}
+            <img src={`./images/${imgName}.png`} alt={room}></img>
+          </div>
+        );
+      })}
     </Container>
   );
 }
@@ -66,37 +88,42 @@ const Container = styled.div`
     flex-direction: column;
     justify-content: center;
     align-items: center;
+    > img {
+      width: 100px;
+    }
   }
 
   > .momRoom {
+    position: relative;
     grid-row: 1/3;
     grid-column: 1/3;
-    border: solid 1px black;
+    border: dotted 1px black;
     grid-area: a;
   }
 
   > .dadRoom {
     grid-row: 1/3;
     grid-column: 1/3;
-    border: solid 1px black;
+    border: dotted 1px black;
     grid-area: b;
   }
 
   > .myRoom {
     grid-row: 2/3;
-    border: solid 1px black;
+    border: dotted 1px black;
     grid-column: 1/2;
     grid-area: d;
   }
 
   > .sisRoom {
+    border: dotted 1px black;
     grid-area: g;
   }
 
   > .livRoom {
     grid-row: 2/4;
     grid-column: 2/4;
+    border: dotted 1px black;
     grid-area: e;
-    background-color: yellow;
   }
 `;
