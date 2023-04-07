d954e7470f84610461e72cc0db99899c09da5533
GiRang Choi <choigirang@kakao.com>
2023-04-07 15:41:01 +0900

    feat: 입력값으로 name지정하기

--- src/components/Main/NameInput.jsx
+++ src/components/Main/NameInput.jsx
@@ -10,9 +10,18 @@ export default function NameInput() {
   // 모달창을 사라지게 할 상태값
   const [nameValue, setNameValue] = useState("");
   // 입력한 input의 value로 RecoilState를 변경하기 위한 상태값
+  const [answerName, setAnswerName] = useState(true);
+  // 이름 입력이 확인 상태
+  const name = /^[가-힣]*$/;
+  // 이름 입력 정규표현식
 
   const nameSubmit = (e) => {
     e.preventDefault();
+    if (!name.test(nameValue)) {
+      setAnswerName(false);
+      console.log(answerName);
+      return;
+    }
     setName(nameValue);
     setNameModal(!nameModal);
   };
@@ -28,26 +37,62 @@ export default function NameInput() {
       <h2>이름을 입력하새오</h2>
       <form onSubmit={nameSubmit}>
         <input type="text" value={nameValue} onChange={nameInput} />
-        <button type="submit">제출하기</button>
+        <button type="submit" className="submitBtn">
+          제출하기
+        </button>
+        {!answerName && <div className="answerName">이름이 이상하지?</div>}
       </form>
     </InputContainer>
   );
 }
-
 const InputContainer = styled.div`
   position: absolute;
+  z-index: 100;
   width: 30vw;
   height: 50vh;
-  background-color: gray;
   color: white;
+  background: linear-gradient(0deg, #ffffff 0%, #b8b8b8 100%);
+  box-shadow: -5px -5px 20px 5px white, 2px 2px 20px 2px black;
   font-size: 2rem;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  z-index: 100;
   transform: translate(-50%, -50%);
   animation: vibrate 0.1s infinite;
+  border-radius: 1rem;
+  overflow: hidden;
+
+  &::before {
+    position: absolute;
+    content: "";
+    display: inline-block;
+    top: -180px;
+    left: 0;
+    width: 30px;
+    height: 100%;
+    background-color: #fff;
+    animation: shiny-btn1 3s ease-in-out infinite;
+  }
+
+  @keyframes shiny-btn1 {
+    0% {
+      -webkit-transform: scale(0) rotate(45deg);
+      opacity: 0;
+    }
+    80% {
+      -webkit-transform: scale(0) rotate(45deg);
+      opacity: 0.5;
+    }
+    81% {
+      -webkit-transform: scale(4) rotate(45deg);
+      opacity: 1;
+    }
+    100% {
+      -webkit-transform: scale(50) rotate(45deg);
+      opacity: 0;
+    }
+  }
 
   @media screen and (max-width: 1200px) {
     width: 25vw;
@@ -68,25 +113,47 @@ const InputContainer = styled.div`
     }
   }
 
-  > input {
-    background: linear-gradient(-45deg, #ee7752, #e73c7e, #23a6d5, #23d5ab);
-    background-size: 400% 400%;
-    animation: AnimationName 15s ease infinite;
+  > form {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
 
-    @keyframes AnimationName {
-      0% {
-        background-position: 0% 50%;
-      }
-      50% {
-        background-position: 100% 50%;
-      }
-      100% {
-        background-position: 0% 50%;
+    > input {
+      background: linear-gradient(-45deg, #ffd7ca, #ffc8dd, #c0eeff, #dbfff7);
+      background-size: 400% 400%;
+      height: 1.5rem;
+      animation: AnimationName 15s ease infinite;
+
+      @keyframes AnimationName {
+        0% {
+          background-position: 0% 50%;
+        }
+        50% {
+          background-position: 100% 50%;
+        }
+        100% {
+          background-position: 0% 50%;
+        }
       }
     }
-  }
 
-  > button {
-    margin-top: 1rem;
+    > .submitBtn {
+      width: 5rem;
+      height: 1.5rem;
+      margin-top: 1rem;
+      margin-bottom: 1rem;
+      border-radius: 5px;
+      box-shadow: 0px 4px #b7bbbc;
+      background-color: #b3b5b0;
+      border: none;
+      color: white;
+      font-family: "Child";
+    }
+
+    > .answerName {
+      font-size: 1.5rem;
+      color: #e93f80;
+    }
   }
 `;
